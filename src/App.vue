0afba083c11260aaaeaa880cccb9eb7df73b5ffa
vasylhulpak <vh@interlogic.com.ua>
2023-07-16 18:20:20 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="p-20px">
+    <a href="https://vitejs.dev" target="_blank" class="flex justify-center h-200px object-cover">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <h1 class="text-center text-32px leading-40px font-bold">Text WindiCSS</h1>
+  </div>
+</template>
